f308b153e71d6d0fdda370776d66a0f23736f7cb
shristi-aiseberg <shristi.gupta@aisepedia.com>
2026-09-06 22:45:11 +0530

    added card and imported in events

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Card from "./components/card";
+import { Event } from "../generated/prisma/client";
 
 const Events = async () => {
   const result = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/events`);
-  const events = await result.json();
-  console.log(events);
-  return <div>Events</div>;
+  const { events } = await result.json();
+    console.log(events);
+  return <div>
+    <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {events.map((event: Event) => (
+        <Card key={event.id} event={event} />
+      ))}
+    </ul>
+  </div>;
 };
 
 export default Events;
